ca98250d842c1d7549e5bd66458da72fc3882ce5
James Perih <james@hotdang.ca>
2016-01-12 21:22:46 -0600

    Allow adding a resume.

--- app/Http/Controllers/ResumesController.php
+++ app/Http/Controllers/ResumesController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Resume;
@@ -14,34 +13,45 @@ use App\Skill;
 use App\Language;
 use App\Hobby;
 
-class ResumesController extends Controller
-{
-    public function get($slug) {
-      $resume = Resume::where('slug', '=', $slug)->first();
-      $id = $resume->id;
+class ResumesController extends Controller {
 
-      $educations = Education::where('resume_id', $id)->orderBy('year', 'desc')->get();
-      $experiences = Experience::where('resume_id', $id)->orderBy('year_end', 'asc')->get();
-      $contactset = ContactSet::where('resume_id', $id)->first();
-      $skills = Skill::where('resume_id', $id)->orderBy('percentage', 'desc')->get();
-      $languages = Language::where('resume_id', $id)->orderBy('percentage', 'desc')->get();
-      $hobbies = Hobby::where('resume_id', $id)->get();
+  public function newResume() {
+    return view('resumes.new');
+  }
 
-      foreach($experiences as $experience) {
-        if ($experience['year_end'] == 0) {
-          $experience['year_end'] = "present";
-        }
+
+  public function store() {
+    $input = Request::all();
+    $resume = Resume::create($input);
+    return redirect()->action('ResumesController@get', $resume->slug);
+  }
+
+  public function get($slug) {
+    $resume = Resume::where('slug', '=', $slug)->first();
+    $id = $resume->id;
+
+    $educations = Education::where('resume_id', $id)->orderBy('year', 'desc')->get();
+    $experiences = Experience::where('resume_id', $id)->orderBy('year_end', 'asc')->get();
+    $contactset = ContactSet::where('resume_id', $id)->first();
+    $skills = Skill::where('resume_id', $id)->orderBy('percentage', 'desc')->get();
+    $languages = Language::where('resume_id', $id)->orderBy('percentage', 'desc')->get();
+    $hobbies = Hobby::where('resume_id', $id)->get();
+
+    foreach($experiences as $experience) {
+      if ($experience['year_end'] == 0) {
+        $experience['year_end'] = "present";
       }
-
-      return view('resumes.show',
-      compact(
-        'resume',
-        'educations',
-        'experiences',
-        'contactset',
-        'skills',
-        'languages',
-        'hobbies'
-      ));
     }
+
+    return view('resumes.show',
+    compact(
+      'resume',
+      'educations',
+      'experiences',
+      'contactset',
+      'skills',
+      'languages',
+      'hobbies'
+    ));
+  }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
 // Route::get('/', 'PagesController@index');
+Route::get('resumes/new', 'ResumesController@newResume');
+Route::post('resumes', 'ResumesController@store');
 Route::get('resumes/{slug}', 'ResumesController@get');
+
 ?>
